3f85271055457abb632f1f75380f4e2c07f23bf4
kaliv0 <kaloyan.ivanov88@gmail.com>
2022-11-16 22:06:41 +0200

    refactored variable names

--- custom-orm/src/main/java/com/kaliv/mapper/ORM.java
+++ custom-orm/src/main/java/com/kaliv/mapper/ORM.java
@@ -38,7 +38,7 @@ public class ORM<T> {
             Field[] declaredFields = classData.getDeclaredFields();
             Field primaryKey = classData.getPrimaryKey();
             String tableName = classData.getTableName();
-            String pKeyType = primaryKey.getType().getName();
+            String primaryKeyType = primaryKey.getType().getName();
 
             StringJoiner joiner = new StringJoiner(Constants.DELIMITER);
             for (Field field : declaredFields) {
@@ -55,18 +55,17 @@ public class ORM<T> {
                     } else if (field.getType() == boolean.class) {
                         columnType = Constants.BIT;
                     }
-                    String col = String.format(Constants.COL_VALUES,
+                    String column = String.format(Constants.COL_VALUES,
                             columnName,
                             columnType,
                             isNullable);
-                    joiner.add(col);
+                    joiner.add(column);
                 }
             }
 
-            String sql = String.format(Constants.CREATE_TABLE_QUERY,
-                    tableName, pKeyType, joiner);
-
-            PreparedStatement statement = connection.prepareStatement(sql);
+            String sqlQuery = String.format(Constants.CREATE_TABLE_QUERY,
+                    tableName, primaryKeyType, joiner);
+            PreparedStatement statement = connection.prepareStatement(sqlQuery);
             statement.executeUpdate();
         }
     }
@@ -74,8 +73,8 @@ public class ORM<T> {
     public void dropTable(final T t) throws SQLException {
         Class<?> clazz = (Class<?>) t;
         String tableName = clazz.getAnnotation(Table.class).name();
-        String sql = String.format(Constants.DROP_TABLE_QUERY, tableName);
-        PreparedStatement statement = connection.prepareStatement(sql);
+        String sqlQuery = String.format(Constants.DROP_TABLE_QUERY, tableName);
+        PreparedStatement statement = connection.prepareStatement(sqlQuery);
         statement.executeUpdate();
     }
 
@@ -89,7 +88,6 @@ public class ORM<T> {
         for (Field field : declaredFields) {
             if (field.isAnnotationPresent(Id.class)) {
                 primaryKey = field;
-
             } else if (field.isAnnotationPresent(Column.class)) {
                 columns.add(field);
                 joiner.add(field.getAnnotation(Column.class).name());
@@ -97,25 +95,24 @@ public class ORM<T> {
         }
 
         int size = columns.size() + 1;
-        String qMarks = IntStream.range(0, size)
+        String questionMarks = IntStream.range(0, size)
                 .mapToObj(e -> "?")
                 .collect(Collectors.joining(Constants.DELIMITER));
 
-        String sql = String.format(Constants.INSERT_QUERY,
+        String sqlQuery = String.format(Constants.INSERT_QUERY,
                 clazz.getAnnotation(Table.class).name(),
                 Objects.requireNonNull(primaryKey).getName(),
                 joiner,
-                qMarks);
-
-        PreparedStatement statement = connection.prepareStatement(sql);
+                questionMarks);
+        PreparedStatement statement = connection.prepareStatement(sqlQuery);
 
         int index = 1;
-        int pkInDb = getPrimaryKeyFromDb(clazz);
+        int primaryKeyFromDb = getPrimaryKeyFromDb(clazz);
         String tableName = clazz.getAnnotation(Table.class).name();
         if (primaryKey.getType() == int.class) {
-            pkInDb++;
-            statement.setInt(index++, pkInDb);
-            primaryKeysInDb.put(tableName, pkInDb);
+            primaryKeyFromDb++;
+            statement.setInt(index++, primaryKeyFromDb);
+            primaryKeysInDb.put(tableName, primaryKeyFromDb);
         }
 
         for (Field field : columns) {
@@ -142,28 +139,27 @@ public class ORM<T> {
                 continue;
             }
             if (field.isAnnotationPresent(Column.class)) {
-                String col = null;
+                String column = null;
                 if (field.getType() == String.class) {
-                    col = String.format(Constants.STRING_VALUE,
+                    column = String.format(Constants.STRING_VALUE,
                             field.getAnnotation(Column.class).name(), field.get(t));
                 } else if (field.getType() == int.class) {
-                    col = String.format(Constants.NUMERIC_VALUE,
+                    column = String.format(Constants.NUMERIC_VALUE,
                             field.getAnnotation(Column.class).name(), (int) field.get(t));
                 } else if (field.getType() == boolean.class) {
-                    col = String.format(Constants.NUMERIC_VALUE,
+                    column = String.format(Constants.NUMERIC_VALUE,
                             field.getAnnotation(Column.class).name(), (boolean) field.get(t) ? 1 : 0);
                 }
-                joiner.add(col);
+                joiner.add(column);
             }
         }
 
-        String sql = String.format(Constants.UPDATE_QUERY,
+        String sqlQuery = String.format(Constants.UPDATE_QUERY,
                 clazz.getAnnotation(Table.class).name(),
                 joiner,
                 Objects.requireNonNull(primaryKey).getName(),
                 id);
-
-        PreparedStatement statement = connection.prepareStatement(sql);
+        PreparedStatement statement = connection.prepareStatement(sqlQuery);
         statement.executeUpdate();
     }
 
@@ -173,14 +169,13 @@ public class ORM<T> {
         Field primaryKey = classData.getPrimaryKey();
         String tableName = classData.getTableName();
 
-        String sql = String.format(Constants.SELECT_QUERY,
+        String sqlQuery = String.format(Constants.SELECT_QUERY,
                 tableName, primaryKey.getName(), id);
+        PreparedStatement statement = connection.prepareStatement(sqlQuery);
+        ResultSet resultSet = statement.executeQuery();
+        resultSet.next();
 
-        PreparedStatement statement = connection.prepareStatement(sql);
-        ResultSet rs = statement.executeQuery();
-        rs.next();
-
-        return readEntityFromDb(clazz, declaredFields, primaryKey, rs);
+        return readEntityFromDb(clazz, declaredFields, primaryKey, resultSet);
     }
 
     public List<T> readAll(final Class<?> clazz) throws Exception {
@@ -189,29 +184,28 @@ public class ORM<T> {
         Field primaryKey = classData.getPrimaryKey();
         String tableName = classData.getTableName();
 
-        String sql = String.format(Constants.SELECT_ALL_QUERY, tableName);
-        PreparedStatement statement = connection.prepareStatement(sql);
-        ResultSet rs = statement.executeQuery();
-        rs.next();
+        String sqlQuery = String.format(Constants.SELECT_ALL_QUERY, tableName);
+        PreparedStatement statement = connection.prepareStatement(sqlQuery);
+        ResultSet resultSet = statement.executeQuery();
+        resultSet.next();
 
         List<T> tList = new ArrayList<>();
-        while (!rs.isAfterLast()) {
-            T entity = readEntityFromDb(clazz, declaredFields, primaryKey, rs);
+        while (!resultSet.isAfterLast()) {
+            T entity = readEntityFromDb(clazz, declaredFields, primaryKey, resultSet);
             tList.add(entity);
-            rs.next();
+            resultSet.next();
         }
         return tList;
     }
 
-    public void delete(final Class<?> clazz, final int l) throws Exception {
+    public void delete(final Class<?> clazz, final int id) throws Exception {
         Container classData = readClassData(clazz);
         Field primaryKey = classData.getPrimaryKey();
         String tableName = classData.getTableName();
 
-        String sql = String.format(Constants.DELETE_QUERY,
-                tableName, primaryKey.getName(), l);
-
-        PreparedStatement statement = connection.prepareStatement(sql);
+        String sqlQuery = String.format(Constants.DELETE_QUERY,
+                tableName, primaryKey.getName(), id);
+        PreparedStatement statement = connection.prepareStatement(sqlQuery);
         statement.executeUpdate();
     }
 
@@ -219,20 +213,19 @@ public class ORM<T> {
     private T readEntityFromDb(final Class<?> tClass,
                                final Field[] declaredFields,
                                final Field primaryKey,
-                               final ResultSet rs) throws Exception {
+                               final ResultSet resultSet) throws Exception {
         T t = (T) tClass.getConstructor().newInstance();
-        int id = rs.getInt(primaryKey.getName());
+        int id = resultSet.getInt(primaryKey.getName());
         primaryKey.setAccessible(true);
         primaryKey.set(t, id);
 
         for (Field field : declaredFields) {
             if (field.isAnnotationPresent(Column.class)) {
                 field.setAccessible(true);
-
                 if (field.getType() == int.class) {
-                    field.set(t, rs.getInt(field.getName()));
+                    field.set(t, resultSet.getInt(field.getName()));
                 } else if (field.getType() == String.class) {
-                    field.set(t, rs.getString(field.getAnnotation(Column.class).name()));
+                    field.set(t, resultSet.getString(field.getAnnotation(Column.class).name()));
                 }
             }
         }
@@ -245,11 +238,11 @@ public class ORM<T> {
             return primaryKeysInDb.get(tableName);
         }
 
-        String sql = String.format(Constants.READ_LAST_PRIMARY_KEY_IN_DB, tableName);
-        PreparedStatement getPrimaryKey = connection.prepareStatement(sql);
-        ResultSet rs = getPrimaryKey.executeQuery();
-        rs.next();
-        return rs.getInt(1);
+        String sqlQuery = String.format(Constants.READ_LAST_PRIMARY_KEY_IN_DB, tableName);
+        PreparedStatement getPrimaryKey = connection.prepareStatement(sqlQuery);
+        ResultSet resultSet = getPrimaryKey.executeQuery();
+        resultSet.next();
+        return resultSet.getInt(1);
     }
 
     private Container readClassData(final Class<?> clazz) {
